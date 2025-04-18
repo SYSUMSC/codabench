106e9bb2f201f91040db6477fe38335c4617d774
xy3 <120182408@qq.com>
2025-04-18 22:21:52 +0800

    强制每个列最高分

--- src/apps/leaderboards/management/commands/auto_select_best_submissions.py
+++ src/apps/leaderboards/management/commands/auto_select_best_submissions.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db.models import Sum, Q
 from django.utils import timezone
 from competitions.models import Submission
-from leaderboards.models import Leaderboard
+from leaderboards.models import Leaderboard, SubmissionScore
 from profiles.models import Organization, User, Membership
 import logging
 
@@ -27,6 +27,11 @@ class Command(BaseCommand):
             action='store_true',
             help='包括有管理员成员的组织（默认跳过）'
         )
+        parser.add_argument(
+            '--force-all-columns',
+            action='store_true',
+            help='强制为每个列选择最佳提交，即使没有主要列的得分'
+        )
 
     def has_admin_members(self, organization):
         """检查组织是否有管理员成员"""
@@ -47,6 +52,7 @@ class Command(BaseCommand):
         dry_run = options.get('dry_run')
         verbose = options.get('verbose')
         include_admin_orgs = options.get('include-admin-orgs')
+        force_all_columns = options.get('force-all-columns')
 
         if dry_run:
             self.stdout.write(self.style.WARNING('执行干运行模式，不会实际修改数据库'))
@@ -120,8 +126,12 @@ class Command(BaseCommand):
                 if not lb_submissions.exists():
                     continue
 
-                # 获取排行榜的主要评分列
+                # 获取排行榜的所有列
                 try:
+                    # 获取排行榜的所有列
+                    columns = leaderboard.columns.all()
+
+                    # 获取主要评分列
                     primary_col = leaderboard.columns.get(index=leaderboard.primary_index)
 
                     # 根据主要评分列的排序方式确定排序方向
@@ -149,6 +159,40 @@ class Command(BaseCommand):
                             # 将最佳提交添加到排行榜
                             best_submission.leaderboard = leaderboard
                             best_submission.save()
+
+                            # 确保该提交有所有列的得分
+                            for column in columns:
+                                # 检查该提交是否已有该列的得分
+                                score_exists = SubmissionScore.objects.filter(
+                                    column=column,
+                                    submission__id=best_submission.id
+                                ).exists()
+
+                                # 如果没有该列的得分，尝试从该组织的其他提交中找到最佳得分
+                                if not score_exists and force_all_columns:
+                                    # 获取该组织在该列上的所有得分
+                                    column_scores = SubmissionScore.objects.filter(
+                                        column=column,
+                                        submission__in=lb_submissions
+                                    )
+
+                                    if column_scores.exists():
+                                        # 根据列的排序方式选择最佳得分
+                                        if column.sorting == 'desc':
+                                            best_score = column_scores.order_by('-score').first()
+                                        else:
+                                            best_score = column_scores.order_by('score').first()
+
+                                        if best_score:
+                                            # 创建一个新的得分记录，关联到最佳提交
+                                            new_score = SubmissionScore.objects.create(
+                                                column=column,
+                                                score=best_score.score
+                                            )
+                                            best_submission.scores.add(new_score)
+
+                                            if verbose:
+                                                self.stdout.write(f'      为提交 ID={best_submission.id} 添加列 {column.title} 的得分: {best_score.score}')
 
                             total_updated += 1
 
